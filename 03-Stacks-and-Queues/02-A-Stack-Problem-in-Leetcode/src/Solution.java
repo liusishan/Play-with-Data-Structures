692154d2c7b35f2e3f790b0958a8cd9426835220
eyao <eyao_9999@163.com>
2019-01-19 21:45:37 +0800

    leetcode 上的 20.Valid Parentheses

---
+++ 03-Stacks-and-Queues/02-A-Stack-Problem-in-Leetcode/src/Solution.java
@@ -0,0 +1,31 @@
+import java.util.Stack;
+
+/**
+ * @Auther: lss
+ * @Date: 2019/1/19 21:15
+ * @Description:
+ */
+class Solution {
+    public boolean isValid(String s) {
+
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '[' || c == '{')
+                stack.push(c);
+            else {
+                if (stack.isEmpty())
+                    return false;
+
+                char topChar = stack.pop();
+                if (c == ')' && topChar != '(')
+                    return false;
+                if (c == ']' && topChar != '[')
+                    return false;
+                if (c == '}' && topChar != '{')
+                    return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
