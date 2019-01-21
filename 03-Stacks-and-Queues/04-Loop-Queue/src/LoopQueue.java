c7fc4b51f14f2f3f3e3111fa8b4cfa217c2e8996
eyao <eyao_9999@163.com>
2019-01-21 18:23:22 +0800

    add loopqueue test

--- 03-Stacks-and-Queues/04-Loop-Queue/src/LoopQueue.java
+++ 03-Stacks-and-Queues/04-Loop-Queue/src/LoopQueue.java
@@ -92,4 +92,16 @@ public class LoopQueue<E> implements Queue<E> {
         res.append("] tail");
         return res.toString();
     }
+
+    public static void main(String[] args) {
+        LoopQueue<Integer> queue = new LoopQueue<>();
+        for (int i = 0; i < 10; i++) {
+            queue.enqueue(i);
+            System.out.println(queue);
+            if (i % 3 == 2) {
+                queue.dequeue();
+                System.out.println(queue);
+            }
+        }
+    }
 }
