ea1a05278c72fd23b8d64ccf591d62fd98294d83
eyao <eyao_9999@163.com>
2019-01-25 20:46:33 +0800

    Test listStack commit.

--- 04-Linked-List/02-Implement-Stack-in-LinkedList/src/LinkedListStack.java
+++ 04-Linked-List/02-Implement-Stack-in-LinkedList/src/LinkedListStack.java
@@ -43,4 +43,15 @@ public class LinkedListStack<E> implements Stack<E>{
         res.append(list);
         return res.toString();
     }
+
+    public static void main(String[] args) {
+
+      LinkedListStack<Integer> listStack = new LinkedListStack<>();
+        for (int i = 0; i < 5; i++) {
+            listStack.push(i);
+            System.out.println(listStack);
+        }
+        listStack.pop();
+        System.out.println(listStack);
+    }
 }
