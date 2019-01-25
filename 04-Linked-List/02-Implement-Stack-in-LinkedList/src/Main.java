82e3e49d26adbe587efd5020b245771ae45e0a1f
eyao <eyao_9999@163.com>
2019-01-25 20:54:35 +0800

    Comparisons between different implementations.

--- 04-Linked-List/02-Implement-Stack-in-LinkedList/src/Main.java
+++ 04-Linked-List/02-Implement-Stack-in-LinkedList/src/Main.java
@@ -1,22 +1,37 @@
+import java.util.Random;
+
 public class Main {
+
+
+    // 测试使用stack运行opCount个push和pop操作所需要的时间，单位：秒
+    private static double testStack(Stack<Integer> stack, int opCount) {
+
+        long startTime = System.nanoTime();
+
+        Random random = new Random();
+        for (int i = 0; i < opCount; i++)
+            stack.push(random.nextInt(Integer.MAX_VALUE));
+        for (int i = 0; i < opCount; i++)
+            stack.pop();
+
+        long endTime = System.nanoTime();
+
+        return (endTime - startTime) / 1000000000.0;
+    }
+
     public static void main(String[] args) {
 
-        LinkedList<Integer> linkedList = new LinkedList<>();
-        for (int i = 0; i < 5; i++) {
-            linkedList.addFirst(i);
-            System.out.println(linkedList);
-        }
+        int opCount = 100000;
 
-        linkedList.add(2,666);
-        System.out.println(linkedList);
+        ArrayStack<Integer> arrayStack = new ArrayStack<>();
+        double time1 = testStack(arrayStack, opCount);
+        System.out.println("ArrayStack, time: " + time1 + " s");
 
-        linkedList.remove(2);
-        System.out.println(linkedList);
+        LinkedListStack<Integer> linkedListStack = new LinkedListStack<>();
+        double time2 = testStack(linkedListStack, opCount);
+        System.out.println("LinkedListStack, time: " + time2 + " s");
 
-        linkedList.removeFirst();
-        System.out.println(linkedList);
-
-        linkedList.removeLast();
-        System.out.println(linkedList);
+        // 其实这个时间比较很复杂，因为LinkedListStack中包含更多的new操作
     }
 }
+
