fd62e906571041a7eb1e68c65b226cc678d7725b
eyao <eyao_9999@163.com>
2019-01-25 20:42:56 +0800

    List implementation stack commit.

---
+++ 04-Linked-List/02-Implement-Stack-in-LinkedList/src/Stack.java
@@ -0,0 +1,17 @@
+/**
+ * @Auther: lss
+ * @Date: 2019/1/19 20:00
+ * @Description:
+ */
+public interface Stack<E>{
+	// 进栈
+	void push(E e);
+	// 出栈
+	E pop();
+	// 查看栈顶元素
+	E peek();
+	//查看栈的元素个数
+	int getSize();
+	// 栈是否为空
+	boolean isEmpty();
+}
