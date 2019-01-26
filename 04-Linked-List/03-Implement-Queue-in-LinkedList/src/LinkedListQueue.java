b37db2290be379340f14c805c91a750efe3b5b4e
eyao <eyao_9999@163.com>
2019-01-26 21:07:48 +0800

    Test LinkedListQueue commit

--- 04-Linked-List/03-Implement-Queue-in-LinkedList/src/LinkedListQueue.java
+++ 04-Linked-List/03-Implement-Queue-in-LinkedList/src/LinkedListQueue.java
@@ -95,4 +95,17 @@ public class LinkedListQueue<E> implements Queue<E> {
         return res.toString();
     }
 
+    public static void main(String[] args) {
+        LinkedListQueue<Integer> queue = new LinkedListQueue<>();
+        for (int i = 0; i < 10; i++) {
+            queue.enqueue(i);
+            System.out.println(queue);
+
+            if (i % 3 == 2) {
+                queue.dequeue();
+                System.out.println(queue);
+            }
+        }
+    }
+
 }
