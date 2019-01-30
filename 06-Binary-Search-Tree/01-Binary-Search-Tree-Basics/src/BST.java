5afe14c8d1958fcdcbd5ed4e6674cfb5b1cb6c59
eyao <2294553196@qq.com>
2019-01-30 21:46:13 +0800

    Binary search tree basics commit

---
+++ 06-Binary-Search-Tree/01-Binary-Search-Tree-Basics/src/BST.java
@@ -0,0 +1,35 @@
+/**
+ * @Auther: lss
+ * @Date: 2019/1/30 21:40
+ * @Description:
+ */
+public class BST<E extends Comparable<E>> {
+
+    private class Node {
+        public E e;
+        public Node left, right;
+
+        public Node(E e) {
+            this.e = e;
+            left = null;
+            right = null;
+        }
+
+    }
+
+    private Node root;
+    private int size;
+
+    public BST() {
+        root = null;
+        size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+}
