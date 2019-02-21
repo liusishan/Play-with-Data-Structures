01b3430311f65f975fc9b65a9b3fe2c4148f265b
eyao <2294553196@qq.com>
2019-02-21 16:40:51 +0800

    LinkedListMap test

--- 07-Set-and-Map/03-Map-Basics/src/LinkedListMap.java
+++ 07-Set-and-Map/03-Map-Basics/src/LinkedListMap.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * @Auther: lss
  * @Date: 2019/2/21 15:37
@@ -113,4 +115,27 @@ public class LinkedListMap<K, V> implements Map<K, V> {
     public boolean isEmpty() {
         return size == 0;
     }
+
+    public static void main(String[] args) {
+
+        System.out.println("Pride and Prejudice");
+
+        ArrayList<String> words = new ArrayList<>();
+        if (FileOperation.readFile("pride-and-prejudice.txt", words)) {
+            System.out.println("Total words: " + words.size());
+
+            LinkedListMap<String, Integer> map = new LinkedListMap<>();
+            for (String word : words) {
+                if (map.contains(word))
+                    map.set(word, map.get(word) + 1);
+                else
+                    map.add(word, 1);
+            }
+
+            System.out.println("Total different words: " + map.getSize());
+            System.out.println("Frequency of PRIDE: " + map.get("pride"));
+            System.out.println("Frequency of PREJUDICE: " + map.get("prejudice"));
+        }
+    }
+
 }
