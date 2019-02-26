7ddad8398810c062e7de4dc56161a5dcc76c55d7
eyao <2294553196@qq.com>
2019-02-26 19:42:43 +0800

    347. Top K Frequent Elements update.

---
+++ 08-Heap-and-Priority-Queue/347-Top-K-Frequent-Elements/src/Solution2.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+/**
+ * @Auther: lss
+ * @Date: 2019/2/26 19:00
+ * @Description:
+ */
+public class Solution2 {
+
+    private class Freq {
+        int e, freq;
+
+        public Freq(int e, int freq) {
+            this.e = e;
+            this.freq = freq;
+        }
+    }
+
+    public List<Integer> topKFrequent(int[] nums, int k) {
+
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for (int num : nums) {
+            if (map.containsKey(num))
+                map.put(num, map.get(num) + 1);
+            else
+                map.put(num, 1);
+        }
+
+        PriorityQueue<Freq> pq = new PriorityQueue<>(new Comparator<Freq>() {
+            @Override
+            public int compare(Freq o1, Freq o2) {
+                return o1.freq - o2.freq;
+            }
+        });
+        for (int key : map.keySet()) {
+            if (pq.size() < k)
+                pq.add(new Freq(key, map.get(key)));
+            else if (map.get(key) > pq.peek().freq) {
+                pq.remove();
+                pq.add(new Freq(key, map.get(key)));
+            }
+        }
+
+        LinkedList<Integer> res = new LinkedList<>();
+        while (!pq.isEmpty())
+            res.add(pq.remove().e);
+        return res;
+    }
+}
