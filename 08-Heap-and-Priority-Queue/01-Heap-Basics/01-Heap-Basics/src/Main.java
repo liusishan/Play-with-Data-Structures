bd374c2341102ac30052242f01ac0c85e2e983a6
eyao <2294553196@qq.com>
2019-02-26 12:28:29 +0800

    MaxHeap test.

--- 08-Heap-and-Priority-Queue/01-Heap-Basics/01-Heap-Basics/src/Main.java
+++ 08-Heap-and-Priority-Queue/01-Heap-Basics/01-Heap-Basics/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 /**
  * @Auther: lss
  * @Date: 2019/2/25 20:50
@@ -6,6 +8,22 @@
 public class Main {
     public static void main(String[] args) {
 
+        int n = 1000000;
+
+        MaxHeap<Integer> maxHeap = new MaxHeap<>();
+        Random random = new Random();
+        for (int i = 0; i < n; i++)
+            maxHeap.add(random.nextInt(Integer.MAX_VALUE));
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = maxHeap.extractMax();
+
+        for (int i = 1; i < n; i++)
+            if (arr[i-1] < arr[i])
+                throw  new IllegalArgumentException("Error");
+
+        System.out.println("Test MaxHeap completed.");
     }
 
 }
