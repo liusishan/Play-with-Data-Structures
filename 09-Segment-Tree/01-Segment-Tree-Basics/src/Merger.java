a38d3800550263b7b1ede1e527a9554c680e0ab9
eyao <2294553196@qq.com>
2019-02-27 21:11:33 +0800

    SegmentTree basics come true two.

---
+++ 09-Segment-Tree/01-Segment-Tree-Basics/src/Merger.java
@@ -0,0 +1,8 @@
+/**
+ * @Auther: lss
+ * @Date: 2019/2/27 20:53
+ * @Description:
+ */
+public interface Merger<E> {
+    E merge(E a, E b);
+}
--- 09-Segment-Tree/01-Segment-Tree-Basics/src/SegmentTree.java
+++ 09-Segment-Tree/01-Segment-Tree-Basics/src/SegmentTree.java
@@ -7,14 +7,37 @@ public class SegmentTree<E> {
 
     private E[] tree;
     private E[] data;
+    private Merger<E> merger;
 
-    public SegmentTree(E[] arr) {
+    public SegmentTree(E[] arr, Merger<E> merger) {
+
+        this.merger = merger;
 
         data = (E[]) new Object[arr.length];
         for (int i = 0; i < arr.length; i++)
             data[i] = arr[i];
 
         tree = (E[]) new Object[4 * arr.length];
+        buildSegmentTree(0, 0, data.length);
+    }
+
+    // 在treeIndex的位置创建表示区间[l...r]的线段树
+    private void buildSegmentTree(int treeIndex, int l, int r) {
+
+        if (l == r) {
+            tree[treeIndex] = data[l];
+            return;
+        }
+
+        int leftTreeIndex = leftChild(treeIndex);
+        int rightTreeIndex = rightChild(treeIndex);
+        // l+r 可能会溢出 所以采用这种
+        int mid = l + (r - l) / 2;
+        buildSegmentTree(leftTreeIndex, l, mid);
+        buildSegmentTree(rightTreeIndex, mid + 1, r);
+
+        tree[treeIndex] = merger.merge(tree[leftTreeIndex], tree[rightTreeIndex]);
+
     }
 
     public int getSize() {
