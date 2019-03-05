96628e1405d276432579ea42d170ad65f8ee1850
eyao <2294553196@qq.com>
2019-03-05 19:28:43 +0800

    303. Range Sum Query - Immutable 第二种思路 commit.

---
+++ 09-Segment-Tree/303-Range-Sum-Query-Immutable/src/NumArray2.java
@@ -0,0 +1,23 @@
+
+/**
+ * @Auther: lss
+ * @Date: 2019/3/5 16:58
+ * @Description:
+ */
+public class NumArray2 {
+
+    private int[] sum; // sum[i]存储前i个元素和
+    // sum[i]存储nums[0...i-1]的和
+
+    public NumArray2(int[] nums) {
+
+        sum = new int[nums.length + 1];
+        sum[0] = 0;
+        for (int i = 1; i < sum.length; i++)
+            sum[i] = sum[i - 1] + nums[i - 1];
+    }
+
+    public int sumRange(int i, int j) {
+        return sum[j + 1] - sum[i];
+    }
+}
